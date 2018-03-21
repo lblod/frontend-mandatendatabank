d7cd222b8107c2005c7f44b8212615d1770bc02c
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-21 22:19:57 +0100

    Cleanup mandataris data table filters

--- app/controllers/bestuur/index.js
+++ app/controllers/bestuur/index.js
@@ -5,61 +5,52 @@ import { isBlank } from '@ember/utils';
 
 export default Controller.extend(DefaultQueryParamsMixin, {
   size: 20,
-  filterPageSize: 100,
+  dropdownPageSize: 10,
   sort: 'is-bestuurlijke-alias-van.achternaam',
-  mandaat_id: '',
-  lijst_id: '',
-  bestuursorgaan: '',
-  beleidsdomein_id: '',
-  mandaten: computed('bestuur.id', 'filterPageSize', function() {
+
+  // selected filter values
+  mandaatId: '',
+  lijstId: '',
+  bestuursorgaanId: '',
+  beleidsdomeinId: '',
+
+  // filter options
+  mandaten: computed('bestuur.id', 'dropdownPageSize', function() {
     return this.get('store').query('mandaat', {
       'filter[bevat-in][is-tijdsspecialisatie-van][bestuurseenheid][id]': this.get('bestuur.id'),
-      'page[size]': this.get('filterPageSize')
+      include: 'bestuursfunctie',
+      'page[size]': this.get('dropdownPageSize')
     });
   }),
-  periodes: computed('bestuur.id', 'filterPageSize', function() {
+  bestuursorganen: computed('bestuur.id', 'dropdownPageSize', function() {
     return this.get('store').query('bestuursorgaan', {
       'filter[is-tijdsspecialisatie-van][bestuurseenheid][id]': this.get('bestuur.id'),
-      'page[size]': this.get('filterPageSize')
+      'page[size]': this.get('dropdownPageSize')
     });
   }),
-  lijsten: computed('bestuur.id', 'bestuursorgaan', 'filterPageSize', function() {
-    let filter = {
-      'filter[rechtstreekse-verkiezing][stelt-samen][is-tijdsspecialisatie-van][bestuurseenheid][id]': this.get('bestuur.id'),
-      'page[size]': this.get('filterPageSize')
-    };
-    if (! isBlank(this.get('bestuursorgaan')))
-      filter['filter[rechtstreekse-verkiezing][stelt-samen][id]'] = this.get('bestuursorgaan');
-    return this.get('store').query('kandidatenlijst', filter);
-  }),
-  beleidsdomeinen: computed('bestuur.id', 'filterPageSize', function() {
+  beleidsdomeinen: computed('bestuur.id', 'dropdownPageSize', function() {
     return this.get('store').query('beleidsdomein-code', {
       'filter[mandatarissen][bekleedt][bevat-in][is-tijdsspecialisatie-van][bestuurseenheid][id]': this.get('bestuur.id'),
-      'page[size]': this.get('filterPageSize')
+      'page[size]': this.get('dropdownPageSize')
     });
   }),
+  lijsten: computed('bestuur.id', 'bestuursorgaanId', 'dropdownPageSize', function() {
+    const filter = {
+      'filter[rechtstreekse-verkiezing][stelt-samen][is-tijdsspecialisatie-van][bestuurseenheid][id]': this.get('bestuur.id'),
+      'page[size]': this.get('dropdownPageSize')
+    };
+    if (! isBlank(this.get('bestuursorgaanId')))
+      filter['filter[rechtstreekse-verkiezing][stelt-samen][id]'] = this.get('bestuursorgaanId');
+    return this.get('store').query('kandidatenlijst', filter);
+  }),
+
   actions: {
     download(file) {
       window.location = file;
     },
-    filterPeriode(periode) {
-      this.set('periode', periode);
-      this.set('bestuursorgaan', periode ? periode.get('id') : '');
-      this.set('page', 0);
-    },
-    filterMandaat(mandaat) {
-      this.set('mandaat', mandaat);
-      this.set('mandaat_id', mandaat ? mandaat.get('id') : '');
-      this.set('page', 0);
-    },
-    filterLijst(lijst) {
-      this.set('lijst', lijst);
-      this.set('lijst_id', lijst ? lijst.get('id') : '');
-      this.set('page', 0);
-    },
-    filterBeleidsdomein(beleidsdomein) {
-      this.set('beleidsdomein', beleidsdomein);
-      this.set('beleidsdomein_id', beleidsdomein ? beleidsdomein.get('id') : '');
+    filter(property, value) {
+      this.set(property, value);
+      this.set(`${property}Id`, value ? value.get('id') : '');
       this.set('page', 0);
     }
   }
--- app/routes/bestuur/index.js
+++ app/routes/bestuur/index.js
@@ -2,55 +2,70 @@ import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
 const filters = [
-  { paramKey: 'mandaat_id',  type: 'mandaat', property: 'mandaat', backendFilter: 'filter[bekleedt][id]' },
-  { paramKey: 'beleidsdomein_id',  type: 'beleidsdomein-code', property: 'beleidsdomein', backendFilter: 'filter[beleidsdomein][id]' },
-  { paramKey: 'bestuursorgaan',  type: 'bestuursorgaan', property: 'periode', backendFilter: 'filter[bekleedt][bevat-in][id]'},
-  { paramKey: 'lijst_id', type: 'kandidatenlijst', property: 'lijst', backendFilter: 'filter[is-bestuurlijke-alias-van][is-kandidaat-voor][id]'}
+  {
+    paramKey: 'mandaatId', type: 'mandaat', ctrlProperty: 'mandaat',
+    backendFilter: 'filter[bekleedt][id]'
+  },
+  {
+    paramKey: 'beleidsdomeinId', type: 'beleidsdomein-code', ctrlProperty: 'beleidsdomein',
+    backendFilter: 'filter[beleidsdomein][id]'
+  },
+  {
+    paramKey: 'bestuursorgaanId', type: 'bestuursorgaan', ctrlProperty: 'bestuursorgaan',
+    backendFilter: 'filter[bekleedt][bevat-in][id]'
+  },
+  {
+    paramKey: 'lijstId', type: 'kandidatenlijst', ctrlProperty: 'lijst',
+    backendFilter: 'filter[is-bestuurlijke-alias-van][is-kandidaat-voor][id]'
+  }
 ];
 
 export default Route.extend(DataTableRouteMixin, {
+  modelName: 'mandataris',
   queryParams: {
     page: { refreshModel: true },
     size: { refreshModel: true },
     sort: { refreshModel: true },
-    mandaat_id: { refreshModel: true },
-    bestuursorgaan: { refreshModel: true },
-    beleidsdomein_id: { refreshModel: true },
-    lijst_id: { refreshModel: true }
+    // Filter query params
+    mandaatId: { refreshModel: true },
+    bestuursorgaanId: { refreshModel: true },
+    beleidsdomeinId: { refreshModel: true },
+    lijstId: { refreshModel: true }
   },
-  modelName: 'mandataris',
   beforeModel(params) {
-    let controller = this.controllerFor(this.routeName);
+    const controller = this.controllerFor(this.routeName);
     controller.set('bestuur', this.modelFor('bestuur'));
-    let qParams = params.queryParams;
-    filters.forEach( filter => this.loadFilter(qParams,filter));
+    const qParams = params.queryParams;
+    filters.forEach( filter => this.setSelectedFilterValue(qParams,filter));
   },
-  loadFilter(qParams, {paramKey, property, type}) {
-    let controller = this.controllerFor(this.routeName);
+  setSelectedFilterValue(qParams, { paramKey, ctrlProperty, type }) {
+    const controller = this.controllerFor(this.routeName);
     if (qParams[paramKey]) {
-      this.get('store').find(type, qParams[paramKey]).then( (result) => controller.set(property, result));
+      this.get('store').find(type, qParams[paramKey]).then( (result) => controller.set(ctrlProperty, result));
     }
     else {
-      this.controllerFor(this.routeName).set(property, null);
+      controller.set(ctrlProperty, null);
     }
   },
   mergeQueryOptions(params) {
     const bestuur = this.modelFor('bestuur');
-    const queryOptions = { 'filter[bekleedt][bevat-in][is-tijdsspecialisatie-van][bestuurseenheid][id]': bestuur.get('id') };
+    const queryOptions = {
+      include: [
+        'is-bestuurlijke-alias-van',
+        'is-bestuurlijke-alias-van.geslacht',
+        'is-bestuurlijke-alias-van.is-kandidaat-voor',
+        'bekleedt',
+        'bekleedt.bestuursfunctie',
+        'heeft-lidmaatschap',
+        'heeft-lidmaatschap.binnen-fractie',
+        'beleidsdomein'
+      ].join(','),
+      'filter[bekleedt][bevat-in][is-tijdsspecialisatie-van][bestuurseenheid][id]': bestuur.get('id')
+    };
     filters.forEach( f => {
       if (params[f.paramKey])
         queryOptions[f.backendFilter] = params[f.paramKey];
     });
-    queryOptions.include = [
-      'is-bestuurlijke-alias-van',
-      'is-bestuurlijke-alias-van.geslacht',
-      'is-bestuurlijke-alias-van.is-kandidaat-voor',
-      'bekleedt',
-      'bekleedt.bestuursfunctie',
-      'heeft-lidmaatschap',
-      'heeft-lidmaatschap.binnen-fractie',
-      'beleidsdomein'
-    ].join(',');
     return queryOptions;
   }
 });
