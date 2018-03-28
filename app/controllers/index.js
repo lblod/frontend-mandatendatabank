faa85681063cc2ab7a9371bd65b5df4abcd94719
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-28 11:09:17 +0200

    Remove deprecated content

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,9 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  sort: 'naam',
   actions: {
     download(file) {
       window.location = file;
-    },
-    selection(row) {
-      this.transitionToRoute('bestuur', row.get('id'));
     }
   }
 });
