b8e40abbf4eef0305f6a9d5648eeb66438526bcc
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-28 11:20:36 +0200

    Hardcode routes on tiles in landing page for demo purposes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,9 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
+    goTo(routeName, id) {
+      this.transitionToRoute(routeName, id);
+    },
     download(file) {
       window.location = file;
     }
