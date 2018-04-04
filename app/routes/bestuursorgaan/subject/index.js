0c3d7226038e1b0497e075a00afc0c450e082b24
Erika Pauwels <erika.pauwels@gmail.com>
2018-04-04 11:13:27 +0200

    Redirect to most recent bestuursorgaan when accessing subject page of niet-tijdsgebonden bestuursorgaan

--- app/routes/bestuursorgaan/subject/index.js
+++ app/routes/bestuursorgaan/subject/index.js
@@ -1,8 +1,16 @@
+import { debug } from '@ember/debug';
 import Route from '@ember/routing/route';
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
 export default Route.extend(DataTableRouteMixin, {
   modelName: 'mandataris',
+  beforeModel() {
+    const bestuursorgaan = this.modelFor('bestuursorgaan.subject');
+    if (!bestuursorgaan.get('isTijdsspecialisatieVan').content) { // niet-tijdsgebonden bestuursorgaan
+      debug('Accessing a niet-tijdsgebonden bestuursorgaan. Redirect to most recent time period.');
+      this.transitionTo('bestuursorgaan.subject.index', '5ab12113496009290c00b465');
+    }
+  },
   mergeQueryOptions(params) {
     const { bestuursorgaan_id } = this.paramsFor('bestuursorgaan.subject');
     return {
