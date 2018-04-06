16730f876e24a6eb3da08b5935cab063b0722999
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-04-06 13:36:20 +0200

    added query params handling

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,13 @@
 import Controller from '@ember/controller';
 import { task, timeout } from 'ember-concurrency';
+import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
   werkingsgebiedId: '',
   bestuurseenheidId: '',
-  bestuurseenheden: null,
-  bestuursorganen: null,
+  werkingsgebied: alias('model.werkingsgebied'),
+  bestuurseenheden: alias('model.bestuurseenheden'),
+  bestuursorganen: alias('model.bestuursorganen'),
 
   searchWerkingsgebied: task(function* (term) {
     yield timeout(250);
@@ -13,31 +15,22 @@ export default Controller.extend({
     return this.get('store').query('werkingsgebied', queryParams);
   }),
 
-  setBestuurseenheden: async function(gebiedId){
-    let queryParams = {'include': 'classificatie',
-                       'filter[werkingsgebied][id]': gebiedId};
-    let bestuurseenheden = await this.get('store').query('bestuurseenheid', queryParams);
-    this.set('bestuurseenheden', bestuurseenheden);
-  },
-
-  setBestuursorganen: async function(bestuurseenheidId){
-    let queryParams = {'include': 'classificatie',
-                       'filter[bestuurseenheid][id]': bestuurseenheidId};
-    let bestuursorganen = await this.get('store').query('bestuursorgaan', queryParams);
-    this.set('bestuursorganen', bestuursorganen);
+  flushQueryParams(){
+    this.set('werkingsgebiedId', '');
+    this.set('bestuurseenheidId', '');
   },
 
   actions: {
     download(file) {
       window.location = file;
     },
-    async listBestuurseenheden(gebied){
+    listBestuurseenheden(gebied){
+      this.flushQueryParams();
+      this.set('werkingsgebied', gebied);
       this.set('werkingsgebiedId', gebied.get('id'));
-      await this.setBestuurseenheden(gebied.get('id'));
     },
-    async listBestuursorganen(bestuurseenheidId){
+    listBestuursorganen(bestuurseenheidId){
       this.set('bestuurseenheidId', bestuurseenheidId);
-      await this.setBestuursorganen(bestuurseenheidId);
     },
     viewBestuursorgaan(bestuursorgaanId){
       this.transitionToRoute('bestuursorgaan.subject', bestuursorgaanId);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,33 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  queryParams: {
+    werkingsgebiedId: { refreshModel: true },
+    bestuurseenheidId: { refreshModel: true }
+  },
+
+  getBestuurseenheden: function(gebiedId){
+    let queryParams = {'include': 'classificatie',
+                       'filter[werkingsgebied][id]': gebiedId};
+    return this.get('store').query('bestuurseenheid', queryParams);
+    },
+
+  getBestuursorganen: function(bestuurseenheidId){
+    let queryParams = {'include': 'classificatie',
+                       'filter[bestuurseenheid][id]': bestuurseenheidId};
+    return this.get('store').query('bestuursorgaan', queryParams);
+  },
+
+  model(params){
+    let modelHash = {werkingsgebied: null, bestuurseenheden: null, bestuursorganen: null};
+    if(params.werkingsgebiedId){
+      modelHash.werkingsgebied = this.get('store').findRecord('werkingsgebied', params.werkingsgebiedId);
+      modelHash.bestuurseenheden = this.getBestuurseenheden(params.werkingsgebiedId);
+    }
+    if(params.bestuurseenheidId){
+      modelHash.bestuursorganen = this.getBestuursorganen(params.bestuurseenheidId);
+    }
+    return RSVP.hash(modelHash);
+  }
 });
