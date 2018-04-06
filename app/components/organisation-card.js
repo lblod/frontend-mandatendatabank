27ed34b4bc14ce901e1216d3a5ea37e66572acc1
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-04-06 17:56:01 +0200

    added display of description of organisation

--- app/components/organisation-card.js
+++ app/components/organisation-card.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['info-tile','info-tile--m', 'info-tile--center', 'info-tile--v-center','grid--equal-height__item'],
   actions: {
     select(organisationId){
       this.get("onSelect")(organisationId);
