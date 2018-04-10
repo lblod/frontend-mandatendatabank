c354c8353099622dccc997e626d2cbd22a5e307c
Niels Vandekeybus <progster@gmail.com>
2018-04-10 20:01:02 +0200

    reset controller state

--- app/routes/bestuursorgaan/subject/administratieve-gegevens.js
+++ app/routes/bestuursorgaan/subject/administratieve-gegevens.js
@@ -1,5 +1,12 @@
 import Route from '@ember/routing/route';
 
+const roles = {
+  '66b95587-b24b-46a5-9231-b4dec06bddac': 'secretaris',
+  '8235802f-37fd-4971-826c-063205a1a31c': 'adjunctSecretaris',
+  'b83a1d0e-0390-4759-871a-e99c9ec00490': 'financieelBeheerder',
+  'f5a7bf88-f31b-4ea8-926b-233b4952fe13': 'informatieAmbtenaar'
+};
+
 export default Route.extend({
   async model() {
     const bestuursorgaan = await this.modelFor('bestuursorgaan.subject');
@@ -10,12 +17,11 @@ export default Route.extend({
   },
   setupController: function(controller, model){
     this._super(controller,model);
-    const roles = {
-      '66b95587-b24b-46a5-9231-b4dec06bddac': 'secretaris',
-      '8235802f-37fd-4971-826c-063205a1a31c': 'adjunctSecretaris',
-      'b83a1d0e-0390-4759-871a-e99c9ec00490': 'financieelBeheerder',
-      'f5a7bf88-f31b-4ea8-926b-233b4952fe13': 'informatieAmbtenaar'
-    };
+    controller.set('secretaris', null);
+    controller.set('adjunctSecretaris', null);
+    controller.set('financieelBeheerder', null);
+    controller.set('informatieAmbtenaar', null);
+    controller.set('zoneChef', null);
     model.get('posities').then( (posities) => {
       posities.forEach( async (positie) => {
         const wordtIngevuldDoor = await positie.get('wordtIngevuldDoor');
