683bfcd1063158a46db4ca9e1153955bab220832
Niels Vandekeybus <progster@gmail.com>
2018-04-11 11:21:08 +0200

    initial rdfa annotations for bestuursorgaan

--- app/models/bestuursorgaan.js
+++ app/models/bestuursorgaan.js
@@ -3,6 +3,7 @@ import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
+  uri: attr(),
   naam: attr(),
   bindingStart: attr('date'),
   bindingEinde: attr('date'),
@@ -11,5 +12,16 @@ export default Model.extend({
   isTijdsspecialisatieVan: belongsTo('bestuursorgaan', { inverse: 'heeftTijdsspecialisaties' }),
   wordtSamengesteldDoor: belongsTo('rechtstreekse-verkiezing', { inverse: 'steltSamen' }),
   heeftTijdsspecialisaties: hasMany('bestuursorgaan', { inverse: 'isTijdsspecialisatieVan' }),
-  bevat: hasMany('mandaat', { inverse: 'bevatIn' })
+  bevat: hasMany('mandaat', { inverse: 'bevatIn' }),
+
+  rdfaBindings: {
+    naam: "http://www.w3.org/2004/02/skos/core#prefLabel",
+    class: "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+    bindingStart: "http://data.vlaanderen.be/ns/mandaat#bindingStart",
+    bindingEinde: "http://data.vlaanderen.be/ns/mandaat#bindingEinde",
+    bestuurseenheid: "http://data.vlaanderen.be/ns/besluit#bestuurt",
+    classificatie: "http://data.vlaanderen.be/ns/besluit#classificatie",
+    isTijdsspecialisatieVan: "http://data.vlaanderen.be/ns/mandaat#isTijdspecialisatieVan",
+    bevat: "http://www.w3.org/ns/org#hasPost"
+  }
 });
--- app/routes/bestuursorgaan/subject/administratieve-gegevens.js
+++ app/routes/bestuursorgaan/subject/administratieve-gegevens.js
@@ -1,12 +1,5 @@
 import Route from '@ember/routing/route';
 
-const roles = {
-  '66b95587-b24b-46a5-9231-b4dec06bddac': 'secretaris',
-  '8235802f-37fd-4971-826c-063205a1a31c': 'adjunctSecretaris',
-  'b83a1d0e-0390-4759-871a-e99c9ec00490': 'financieelBeheerder',
-  'f5a7bf88-f31b-4ea8-926b-233b4952fe13': 'informatieAmbtenaar'
-};
-
 export default Route.extend({
   async model() {
     const bestuursorgaan = await this.modelFor('bestuursorgaan.subject');
