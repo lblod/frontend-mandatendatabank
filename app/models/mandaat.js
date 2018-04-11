bf67eb932086b65914d5352655378bfc02124e9d
Niels Vandekeybus <progster@gmail.com>
2018-04-11 12:03:31 +0200

    include link to "mandaat" in rdfa

--- app/models/mandaat.js
+++ app/models/mandaat.js
@@ -3,6 +3,7 @@ import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
 
 export default Model.extend({
+  uri: attr(),
   aantalHouders: attr(),
   bestuursfunctie: belongsTo('bestuursfunctie-code', { inverse: null }),
   bevatIn: belongsTo('bestuursorgaan', { inverse: 'bevat' })
