a9e76907bc459a24bf5f524dada43abaeaa98f2e
Miet Claes <miet@miet.be>
2018-09-17 09:42:15 +0200

    Remove broken cards for mandatendatabank

--- app/components/organisation-card.js
+++ app/components/organisation-card.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['info-tile','info-tile--m', 'info-tile--center', 'info-tile--v-center','grid--equal-height__item'],
+  classNames: ['info-tile','info-tile--m', 'info-tile--center'],
   classNameBindings: ['isSelected:selected-organisation-card'],
   isSelected: false,
   actions: {
