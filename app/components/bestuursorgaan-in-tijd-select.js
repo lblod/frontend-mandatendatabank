4d24c076a21a35532b768f1a8b87a3f914d21aa6
Claire Lovisa <lovisa.claire@gmail.com>
2018-12-27 18:13:52 +0100

    Use an ember-power-select to choose the bestuursorgaan

--- app/components/bestuursorgaan-in-tijd-select.js
+++ app/components/bestuursorgaan-in-tijd-select.js
@@ -1,12 +1,18 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { alias, sort } from '@ember/object/computed';
 
 export default Component.extend({
   router: service(),
+  bestuursorganen: alias('model.heeftTijdsspecialisaties'),
+  bestuursorganenSortingDesc: Object.freeze(['bindingStart:desc']),
+  descSortedBestuursorganen: sort('bestuursorganen', 'bestuursorganenSortingDesc'),
+  selectedBestuursorgaan: alias('lastMandaat'),
 
   actions: {
-    transitionToSubject(id) {
-      this.router.transitionTo('bestuursorgaan.subject.index', id);
-    }
+    select(selectedBestuursorgaan) {
+      this.set('selectedBestuursorgaan', selectedBestuursorgaan);
+      this.router.transitionTo('bestuursorgaan.subject.index', selectedBestuursorgaan.id);
+    },
   }
 });
