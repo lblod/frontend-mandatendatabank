caa09afcba33dbae53517648f2133afce16d7807
Claire Lovisa <lovisa.claire@gmail.com>
2019-02-26 10:25:40 +0100

    Update Ember v3.7

--- tests/integration/components/bestuursorgaan-in-tijd-select-test.js
+++ tests/integration/components/bestuursorgaan-in-tijd-select-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | bestuursorgaan-in-tijd-select', function(hooks
 
     await render(hbs`{{bestuursorgaan-in-tijd-select}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | bestuursorgaan-in-tijd-select', function(hooks
       {{/bestuursorgaan-in-tijd-select}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
