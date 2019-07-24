44a864aa788f4a073969e9a4b8ffa97c8f209682
Erika Pauwels <erika.pauwels@gmail.com>
2019-07-24 16:13:08 +0200

    Defer rendering of download miniatures in fastboot

--- app/components/download-miniatures.js
+++ app/components/download-miniatures.js
@@ -5,19 +5,25 @@ import { computed } from '@ember/object';
 export default Component.extend({
   classNames: ['grid'],
   store: service(),
+  fastboot: service(),
   init() {
     this._super(...arguments);
-    this.fetchMetadata('text/turtle', 'ttlFile');
-    this.fetchMetadata('text/csv', 'csvFile');
+    const promises = Promise.all([
+      this.fetchMetadata('text/turtle', 'ttlFile'),
+      this.fetchMetadata('text/csv', 'csvFile')
+    ]);
+
+    if (this.fastboot.isFastBoot)
+      this.fastboot.deferRendering(promises);
   },
   async fetchMetadata(mimeType, field) {
     try {
-      const ttlFiles = await this.store.query('export', {
+      const files = await this.store.query('export', {
         sort: '-created',
         filter: { format: mimeType },
         page: { size: 1 }
       });
-      this.set(field, ttlFiles.firstObject);
+      this.set(field, files.firstObject);
     }
     catch(e) {
       // not handling it at the moment
