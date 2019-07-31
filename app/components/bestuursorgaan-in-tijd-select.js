4fd6ed11cebe7856615628de8ab9adf370366662
Erika Pauwels <erika.pauwels@gmail.com>
2019-07-31 17:44:25 +0200

    Don't use moment-format when running fastboot

--- app/components/bestuursorgaan-in-tijd-select.js
+++ app/components/bestuursorgaan-in-tijd-select.js
@@ -1,9 +1,12 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { sort } from '@ember/object/computed';
+import { reads, sort } from '@ember/object/computed';
 
 export default Component.extend({
   router: service(),
+  fastboot: service(),
+
+  isFastBoot: reads('fastboot.isFastBoot'),
   bestuursorganenSortingDesc: Object.freeze(['bindingStart:desc']),
   descSortedBestuursorganen: sort('bestuursorgaan.heeftTijdsspecialisaties', 'bestuursorganenSortingDesc'),
 
--- app/controllers/bestuursorgaan/subject/index.js
+++ app/controllers/bestuursorgaan/subject/index.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 export default Controller.extend({
+  fastboot: service(),
+  isFastBoot: reads('fastboot.isFastBoot'),
   sort: 'is-bestuurlijke-alias-van.achternaam',
   page: 0,
   size: 20
