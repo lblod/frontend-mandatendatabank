b283b4adcdffcb55902f950fd5106d498e15048c
achraf <achrafatauil@hotmail.com>
2020-12-11 12:02:49 +0100

    upgrade download-miniatures component-class to octane syntax

--- app/components/download-miniatures.js
+++ app/components/download-miniatures.js
@@ -1,32 +1,42 @@
-import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { action, computed } from '@ember/object';
 
-export default Component.extend({
-  store: service(),
-  init() {
-    this._super(...arguments);
+export default class DownloadMiniaturesComponent extends Component {
+
+  constructor() {
+    super(...arguments);
+    this.queryCsv()
+    this.queryTtl()
+  }
+
+  queryCsv(){
     this.store.query('export', {
       sort: '-created',
       filter: { format: 'text/turtle' },
       page: { size: 1 }
     }).then((files) => this.set('ttlFile', files.get('firstObject')));
+  }
+
+  queryTtl(){
     this.store.query('export', {
       sort: '-created',
       filter: { format: 'text/csv' },
       page: { size: 1 }
     }).then((files) => this.set('csvFile', files.get('firstObject')));
-  },
-  ttlMetadata: computed('ttlFile', function() {
-    return `Turtle - ${this.get('ttlFile.filesizeMb')}MB - ${this.get('ttlFile.createdFormatted')}`;
-  }),
-  csvMetadata: computed('csvFile', function() {
-    return `CSV - ${this.get('csvFile.filesizeMb')}MB - ${this.get('csvFile.createdFormatted')}`;
-  }),
-  actions: {
-    download(file) {
-      if (file)
-        window.location = `/files/${file.get('filename')}`;
+  }
+
+  @computed('ttlFile.{filesizeMb,createdFormatted}', 'csvFile.{filesizeMb,createdFormatted}')
+  get ttlMetadata(){
+    return `Turtle - ${this.ttlFile.filesizeMb}MB - ${this.ttlFile.createdFormatted}`;
+  }
+
+  get csvMetadata(){
+    return `CSV - ${this.csvFile.filesizeMb}MB - ${this.csvFile.createdFormatted}`;
+  }
+
+  @action
+  download(file) {
+    if (file)
+      window.location = `/files/${file.get('filename')}`;
     }
   }
-});
