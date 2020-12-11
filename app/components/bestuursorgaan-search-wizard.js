291e0da7f2704e8081dd5de7266422d8dced1a85
achraf <achrafatauil@hotmail.com>
2020-12-11 12:52:48 +0100

    add forgotten args. in bestuursorgaan-search-wizard component-class

--- app/components/bestuursorgaan-search-wizard.js
+++ app/components/bestuursorgaan-search-wizard.js
@@ -6,24 +6,24 @@ import { action } from '@ember/object';
 export default class BestuursorgaanSearchWizardComponent extends Component {
   @tracked selectedBestuurseenheidId = null;
   @tracked selectedBestuursorgaanId = null;
-  @alias('model.werkingsgebied') werkingsgebied;
-  @sort('model.bestuurseenheden', 'generalSort') bestuurseenheden; 
-  @sort('model.bestuursorganen', 'generalSort') bestuursorganen;
+  @alias('args.model.werkingsgebied') werkingsgebied;
+  @sort('args.model.bestuurseenheden', 'generalSort') bestuurseenheden; 
+  @sort('args.model.bestuursorganen', 'generalSort') bestuursorganen;
   @tracked generalSort = Object.freeze(['classificatie.label']);
 
   @action
   listBestuurseenheden(gebied){
-    return this.onListBestuurseenheden(gebied);
+    this.args.onListBestuurseenheden(gebied);
   }
     
   @action
   listBestuursorganen(bestuurseenheidId){
-    return this.onListBestuursorganen(bestuurseenheidId);
+    this.args.onListBestuursorganen(bestuurseenheidId);
   }
 
   @action
   viewBestuursorgaan(bestuursorgaanId){
-    return this.onViewBestuursorgaan(bestuursorgaanId);
+    this.args.onViewBestuursorgaan(bestuursorgaanId);
   }
 }
 
