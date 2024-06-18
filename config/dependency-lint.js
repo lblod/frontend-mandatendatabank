b095f071ee5919c986f6bfb587c0526e39e70052
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-06-18 17:29:36 +0200

    Resolve some dependency-lint issues
    ember-rdfa-helpers brings in ember-modifier v3, but we don't use the
    modifier so we ignore the linting error until we remove that addon in
    the future.

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = {
-  generateTests: false
+  generateTests: false,
+  allowedVersions: {
+    'ember-modifier': '*', // ember-rdfa-helpers depends on ember-modifier v3, but we don't use the modifier so we ignore the lint issue until we move away from that addon
+  },
 };
