be5152defdd522bb02037c9fc56d8b40182279a0
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-03-24 17:13:59 +0100

    Resolve an EmberData deprecation

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,6 +11,11 @@ module.exports = function (defaults) {
         },
       },
     },
+    emberData: {
+      deprecations: {
+        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+      },
+    },
   });
 
   return app.toTree();
