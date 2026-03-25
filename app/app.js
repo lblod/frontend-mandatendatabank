b287b6c5b1c28eae03c4b83b48c2f5f350cae0e2
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-03-25 11:13:36 +0100

    Update to Ember v6.7
    This is the last version we can update to to stay on the classic, non-Vite blueprint.

--- app/app.js
+++ app/app.js
@@ -3,6 +3,11 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import { registerInflectorRules } from './config/custom-inflector-rules';
 import config from 'frontend-mandatendatabank/config/environment';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 registerInflectorRules();
 
